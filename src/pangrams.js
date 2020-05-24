73ffd813dfd1f8f07ea0e718dc970db3151481e5
Ender Smith <ender@prescryptive.com>
2020-05-23 22:43:37 -0700

    Comicbesh Sans

--- src/pangrams.js
+++ src/pangrams.js
@@ -1,16 +1,20 @@
 // by Ender Smith, Editor-in-Chief, AurekFonts
 
 const pangrams = {
-  JediWisdom: 'A Jedi acknowledges pride, for quiet humility belies unexorcized vanity.',
-  Podracing: 'How many explosions did Jezka survive before she quit podracing?',
-  BlackholeDigraphs: 'Blackhole, shining dark mid aether, wax cool and quiet high above a joyless chaos of neon zaps.',
-  Homie: 'Anakin<br>turn  the  ship<br>around  and  go<br>back  homie<br>right  awayl',
-  short: 'Use the Force!',
-  JarJarFez: 'Jar Jar Binks quietly removes his X-Wing-capped fez.',
-  Tydirium: 'TRANS MITCOD ECLEA RANCET ODEAC TIVATE SHIEL DGENER ATOR.',
-  TydiriumEnglish: 'TRANS MIT CODE CLEAR ANCE TO DE ACT I VATE SHIELD GEN ER A TOR.',
-  JazzyFox: 'The Emperor cackled whilst bequeathing Vader a jazzy fox',
-  Jedha: 'THEFORCESURROUNDSBINDSUS',
-  BanthaMilk: 'Five banthas graze placidly, quixotic jawas milk.',
-  BSO: 'Warning: jezfly quivexium prohibited in Black Spire.'
+  short:              'Use the Force!',
+  Jedha:              'THEFORCESURROUNDSBINDSUS',
+  JarJarFez:          'Jar Jar Binks quietly removes his X Wing fez cap',
+  BanthaMilk:         'Five banthas graze placidly, quixotic jawas milk.',
+  BSO:                'Warning: jezfly quivexium prohibited in Black Spire.',
+  Tydirium:           'TRANS MITCOD ECLEA RANCET ODEAC TIVATE SHIEL DGENER ATOR.',
+  JazzyFox:           'The Emperor cackled whilst bequeathing Vader a jazzy fox',
+  TydiriumEnglish:    'TRANS MIT CODE CLEAR ANCE TO DE ACT I VATE SHIELD GEN ER A TOR.',
+  Podracing:          'How many explosions did Jezka survive before she quit podracing?',
+  JediWisdom:         'A Jedi acknowledges pride, for quiet humility belies unexorcized vanity.',
+  Homie:              'Anakin<br>turn  the  ship<br>around  and  go<br>back  homie<br>right  awayl',
+  BlackholeDigraphs:  'Blackhole, shining dark mid aether, wax cool and quiet high above a joyless chaos of neon zaps.',
+
+  Prequels:           'Acquiescent Jedi-gazes allow a phantom to vex and betray folk.',
+  Original:           'A farmboy escapes quiet killjoys, pilots X-Wing, vaporizes Death Star.',
+  Sequels:            'Ex-trooper joins a nobody junk collector to fight a sequelized Vader-wimp.'
 }
