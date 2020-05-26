e63adc4280af104f91440fd6211a61d53b703b82
Ender Smith <ender@prescryptive.com>
2020-05-26 15:26:08 -0700

    nbsp

--- src/globals.js
+++ src/globals.js
@@ -8,7 +8,7 @@ const linkHeader =
     <a href="${pathname}">[Home]</a>&nbsp;&nbsp;&nbsp;
     <a href="${pathname}?sort=license">[Fonts]</a>&nbsp;&nbsp;&nbsp;
     <a href="${pathname}?sort=script">[Alphabets]</a>&nbsp;&nbsp;&nbsp;
-    <a href="${pathname}?sort=foundry">[Foundries / Artists]</a>&nbsp;&nbsp;&nbsp;
-    <a href="${pathname}#contact-us">[Contact Us]</a>
+    <a href="${pathname}?sort=foundry">[Foundries&nbsp;/&nbsp;Artists]</a>&nbsp;&nbsp;&nbsp;
+    <a href="${pathname}#contact-us">[Contact&nbsp;Us]</a>
   </p>`;
 const homeBanner = `<p><a href="${pathname}"><img src="./AurekFontsBanner.png" alt="AurekFonts Home" title="AurekFonts Home"/></a></p>`;
