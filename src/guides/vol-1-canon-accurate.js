be60c905c5288876be14a5c8bcad0f921f5827fc
Ender Smith <ender@prescryptive.com>
2020-05-27 18:17:30 -0700

    blue

--- src/guides/vol-1-canon-accurate.js
+++ src/guides/vol-1-canon-accurate.js
@@ -55,7 +55,7 @@ const canonAccurate =
       Following the comparison, we will make and give our recommendations.
     </p>
     <img
-      src="src/guides/AurebeshComparisonImages/AurebeshComparisonWhite.png"
+      src="src/guides/AurebeshComparisonImages/AurebeshComparisonBlue.png"
       alt="Comparison of plain Aurebesh fan-fonts against canonical reference"
       title="Comparison of plain Aurebesh fan-fonts against canonical reference"
     />
