25089ef80f5553697f73ba4fa9800316a14ef698
Ender Smith <ender@prescryptive.com>
2020-06-12 16:38:59 -0700

    TF Gunray

--- src/globals.js
+++ src/globals.js
@@ -5,10 +5,10 @@ const pathname = window.location.pathname;
 
 const linkHeader = 
   `<p id="link-header">
-    <a href="${pathname}">[Home]</a>&nbsp;&nbsp;&nbsp;
-    <a href="${pathname}?sort=license">[Fonts]</a>&nbsp;&nbsp;&nbsp;
-    <a href="${pathname}?sort=script">[Alphabets]</a>&nbsp;&nbsp;&nbsp;
-    <a href="${pathname}?sort=foundry">[Foundries&nbsp;/&nbsp;Artists]</a>&nbsp;&nbsp;&nbsp;
-    <a href="${pathname}#contact-us">[Contact&nbsp;Us]</a>
+    <span><a href="${pathname}">[Home]</a>&nbsp;&nbsp;&nbsp;</span>
+    <span><a href="${pathname}?sort=license">[Fonts]</a>&nbsp;&nbsp;&nbsp;</span>
+    <span><a href="${pathname}?sort=script">[Alphabets]</a>&nbsp;&nbsp;&nbsp;</span>
+    <span><a href="${pathname}?sort=foundry">[Foundries&nbsp;/&nbsp;Artists]</a>&nbsp;&nbsp;&nbsp;</span>
+    <span><a href="${pathname}#contact-us">[Contact&nbsp;Us]</a></span>
   </p>`;
 const homeBanner = `<p><a href="${pathname}"><img src="./AurekFontsBanner.png" alt="AurekFonts Home" title="AurekFonts Home"/></a></p>`;
