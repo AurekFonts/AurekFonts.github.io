7b61655355bddc843b188932f04fb9ea6b14837e
Ender Smith <ender@prescryptive.com>
2020-07-14 20:51:00 -0700

    stuff

---
+++ src/static-screens/collections.js
@@ -0,0 +1,201 @@
+// by Ender Smith, Editor-in-Chief, AurekFonts
+
+const scriptSortScreen = (totalParam) => linkHeader +
+homeBanner +
+`<h1>
+  Collections
+</h1>
+<p>
+  These are currated collections of recommended fonts.
+</p>
+<h2>Canon Scripts / Alphabets</h2>
+<ul>
+  <li>
+    <h3>Modern Aurebesh</h3>
+    <ul>
+      <li>
+        <a href="${pathname}?script=Aurebesh">
+          Aurebesh
+        </a>
+      </li>
+      <li>
+          Dishabesh (coming soon)
+      </li>
+      <li>
+        <a href="${pathname}?script=Droidobesh">
+          Droidobesh
+        </a>
+      </li>
+      <li>
+        <a href="${pathname}?script=ImperialCode">
+          "Imperial Code" [unidentified Aurebesh variant]
+        </a>
+      </li>
+      <li>
+        <a href="${pathname}?script=NabooAurebesh">
+          "Naboo Aurebesh"
+        </a>
+      </li>
+    </ul>
+  </li>
+  <li>
+    <h3>Aurebesh Ancestors</h3>
+    <ul>
+      <li>
+        <a href="${pathname}?script=Domabesh">
+          Domabesh
+        </a>
+      </li>      
+      <li>
+        <a href="${pathname}?script=OldTongue">
+          "The Old Tongue" / Ur-Kittât
+        </a>
+      </li>
+      <li>
+        <a href="${pathname}?script=Protobesh">
+          Protobesh / Coremaic
+        </a>
+      </li>
+    </ul>
+  </li>
+  <li>
+    <h3>High Galactic Alphabet</h3>
+    <ul>
+      <li>
+        <a href="${pathname}?script=HighGalactic">
+          Diagetic [in-universe]
+        </a>
+      </li>
+      <li>
+        <a href="${pathname}?script=Pseudobesh">
+          "Pseudobesh" [styled to look like Aurebesh]
+        </a>
+      </li>
+      <li>
+        <a href="${pathname}?script=Numeric">
+          "Numeric" [Numeral-specific fonts]
+        </a>
+      </li>
+      <li>
+        Titles / Crawls / Etc. (coming soon)
+      </li>
+    </ul>
+  </li>
+  <li>
+    <h3>Corellia</h3>
+    <ul>
+      <li>
+        <a href="${pathname}?script=Clynese">
+          Clynese
+        </a>
+      </li>
+    </ul>
+  </li>
+  <li>
+    <h3>Geonosis</h3>
+    <ul>
+      <li>
+        <a href="${pathname}?script=Geonosian">
+          Geonosian
+        </a>
+      </li>
+    </ul>    
+  </li>
+  <li>
+    <h3>Mandalore</h3>
+    <ul>
+      <li>
+        <a href="${pathname}?script=Mandalorian">
+          Mandalorian
+        </a>
+      </li>
+    </ul>
+  </li>
+  <li>
+    <h3>Naboo</h3>
+    <ul>
+      <li>
+        <a href="${pathname}?script=NabooFuthark">
+          Futhark
+        </a>
+      </li>
+      <li>
+        <a href="${pathname}?script=NabooFuthork">
+          Futhork
+        </a>
+      </li>
+    </ul>
+  </li>
+  <li>
+    <h3>The Outer Rim</h3>
+    <ul>
+      <li>
+        Huttese (coming soon)
+      </li>
+      <li>
+        <a href="${pathname}?script=OuterRim">
+          Outer Rim Basic [not to be confused with Huttese]
+        </a>
+      </li>
+    </ul>
+  </li>
+  <li>
+    <h3>Trade Federation</h3>
+    <ul>
+      <li>
+        <a href="${pathname}?script=TradeFederation">
+          Trade Federation Basic
+        </a>
+      </li>
+    </ul>
+  </li>
+  <li>
+    <h3>Unknown canon scripts</h3>
+    <ul>
+      <li>
+        <a href="${pathname}?script=Kitisakkullian">
+          "Kitisakkullian" [unidentified canon Jedi Text script]
+        </a>
+      </li>
+      <li>
+        <a href="${pathname}?script=Ubese">
+        "Ubese" [unidentified script from Boushh's costume]
+        </a>
+      </li>
+      <li>
+        <a href="${pathname}?script=UnknownCanon">
+          Unknown canon scripts
+        </a>
+      </li>
+    </ul>
+  </li>
+</ul>
+<h2><em>Legends</em> (non-canon) Scripts / Alphabets</h2>
+<ul>
+  <li>
+    <a href="${pathname}?script=Atrisian">
+      <h3>Atrisian</h3>
+    </a>
+  </li>
+  <li>
+    <a href="${pathname}?script=Ewokese">
+      <h3>Ewokese</h3>
+    </a>
+  </li>
+  <li>
+    <a href="${pathname}?script=Kittat">
+      <h3>Sith Kittât</h3>
+    </a>
+  </li>
+  <li>
+    <a href="${pathname}?script=JabbasAlphabet">
+      <h3>'Jabba's Alphabet' [unidentified <em>Legends</em> script from 1997 Pizza Hut boxes]</h3>
+    </a>
+  </li>
+  <li>
+    <a href="${pathname}?script=UnknownLegends">
+      <h3>Unknown <em>Legends</em> scripts</h3>
+    </a>
+  </li>
+</ul>
+`
