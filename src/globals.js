1ba76e2c85c267759b6a2a6aecfac41175638efb
Ender Smith <ender@happyleviathan.com>
2021-01-02 21:12:12 -0800

    header

--- src/globals.js
+++ src/globals.js
@@ -9,6 +9,8 @@ const linkHeader =
     <span><a href="${pathname}?sort=license">[Fonts]</a>&nbsp;&nbsp;&nbsp;</span>
     <span><a href="${pathname}?sort=script">[Alphabets]</a>&nbsp;&nbsp;&nbsp;</span>
     <span><a href="${pathname}?sort=foundry">[Foundries&nbsp;/&nbsp;Artists]</a>&nbsp;&nbsp;&nbsp;</span>
+    <span><a href="https://twitter.com/AurekFonts">[Twitter]</a>&nbsp;&nbsp;&nbsp;</span>
+    <span><a href="https://aurekfonts.tumblr.com/">[Blog]</a>&nbsp;&nbsp;&nbsp;</span>
     <span><a href="${pathname}#contact-us">[Contact&nbsp;Us]</a></span>
   </p>`;
 const homeBanner = `<p><a href="${pathname}"><img src="./AurekFontsBanner.png" alt="AurekFonts Home" title="AurekFonts Home"/></a></p>`;
