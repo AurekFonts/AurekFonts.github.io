bfb71eff4dc741cc605d57e218d422449c78fee0
Ender Smith <ender@happyleviathan.com>
2021-01-18 09:38:01 -0800

    Aurebesh Vectors

--- src/pangrams.js
+++ src/pangrams.js
@@ -3,19 +3,19 @@
 const pangrams = {
   short:              'Use the Force!',
   Jedha:              'THEFORCESURROUNDSBINDSUS',
-  JarJarFez:          'Jar Jar Binks quietly removes his X Wing fez cap',
-  BanthaMilk:         'Quixotic jawas milk five placidly grazing banthas.',
-  BSO:                'Warning: jezfly quivexium prohibited in Black Spire.',
+  JarJarFez:          'Jar Jar Binks quietly removed his X Wing fez cap',
+  BanthaMilk:         'Quixotic jawas milk five placidly grazing banthas',
+  BSO:                'Warning: jezfly quivexium prohibited in Black Spire',
   Tydirium:           'TRANS MITCOD ECLEA RANCET ODEAC TIVATE SHIEL DGENER ATOR.',
   JazzyFox:           'The Emperor cackled whilst bequeathing Vader a jazzy fox',
   TydiriumEnglish:    'TRANS MIT CODE CLEAR ANCE TO DE ACT I VATE SHIELD GEN ER A TOR.',
   Podracing:          'How many explosions did Jezka survive before she quit podracing?',
-  JediWisdom:         'A Jedi acknowledges pride, for quiet humility belies unexorcized vanity.',
+  JediWisdom:         'A Jedi acknowledges pride, for quiet humility belies unexorcized vanity',
   Homie:              'Anakin<br>turn  the  ship<br>around  and  go<br>back  homie<br>right  awayl',
   BlackholeDigraphs:  'Blackhole, shining dark mid aether, wax cool and quiet high above a joyless chaos of neon zaps.',
   Yoda:               '"Judge me by my size," Yoda inquired of his vexed apprentice, Luke Skywalker.',
 
-  Prequels:           'Acquiescent Jedi-gazes allow a phantom to vex and betray folk.',
-  Original:           'A farmboy escapes quiet killjoys, pilots X-Wing, vaporizes Death Star.',
-  Sequels:            'Ex-trooper joins a nobody junk collector to fight a sequelized Vader-wimp.'
+  Prequels:           'Acquiescent Jedi-gazes allow a phantom fascist to vex, kill, & betray',
+  Original:           'A farmboy escapes quiet killjoys, pilots X-Wing, vaporizes Death Star',
+  Sequels:            'Ex-trooper joins a nobody junk collector to fight a wimpy, sequelized Vader'
 }
